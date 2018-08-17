80bbe3b5d83f28ce423f64b76e231c15d715c1f5
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-08-17 22:58:49 +0300

    added app.js

--- application/babel/app.js
+++ application/babel/app.js
@@ -1,3 +1,35 @@
-var message = '"app.js" is loaded'
+var Form = {
+  //  DATA
+  data: function() {
+    return {
+      from: $('[name=from]').val(),
+      subject: $('[name=subject]').val(),
+      to: JSON.parse($('[name=to]').val()),
+      message: $('[name=message]').val()
+    }
+  },
 
-console.log(message)
+  //  SUBMIT
+  submit: function() {
+    $.ajax({
+      type: 'POST',
+      url: 'includes/php/form.php',
+      data: {dd: JSON.stringify(Form.data())},
+      dataType: 'json',
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        $('#callback').text('error')
+      },
+      success: function(data) {
+        $('[name=submit]').remove()
+        $('#callback').text(data.status)
+      }
+    })
+  }
+}
+
+//  DOCUMENT READY
+$(document).ready(function() {
+  $('[name=submit]').on('click', function() {
+    Form.submit()
+  })
+})
